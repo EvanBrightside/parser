7a57f922df2033b94d1de5fe3ab9d95c209daf12
Ivan Zabrodin <xvanx84@gmail.com>
2016-04-24 18:42:17 +0300

    Sidekiq

---
+++ app/jobs/subcategories_job.rb
@@ -0,0 +1,12 @@
+require 'open-uri'
+
+class SubcategoriesJob < ActiveJob::Base
+
+  def perform(page_url)
+    doc = Nokogiri::HTML(open(page_url))
+
+    doc.css('#_title').each do |elem|
+      ParseJob.perform_later(elem.attr('href'))
+    end
+  end
+end
